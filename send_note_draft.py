0a73acaf5fab37f135475dc9f6812048eb2465bf
openceo2025 <openceo99@gmail.com>
2025-07-31 22:17:06 +0900

    note account change

--- send_note_draft.py
+++ send_note_draft.py
@@ -8,7 +8,7 @@ IMAGE_PATH = "example/b6701f05-1e2e-4776-a7c3-69c13c469514.png"  # replace with 
 
 def main():
     payload = {
-        "account": "default",
+        "account": "nicchi",
         "content": CONTENT,
         "images": [IMAGE_PATH],
     }
